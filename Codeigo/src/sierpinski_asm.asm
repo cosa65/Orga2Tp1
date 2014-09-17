6c151065982c34693305c57b57404315ad969390
cosa65 <mfosc65@gmail.com>
2014-09-17 15:15:44 -0300

    rgnrew

--- Codeigo/src/sierpinski_asm.asm
+++ Codeigo/src/sierpinski_asm.asm
@@ -4,40 +4,47 @@ section .data
 
 section .text
 
-;void sierpinski_asm (unsigned char *src,			;rdi
-;                     unsigned char *dst,			;rsi
-;                     int cols, int rows,			;rdx, rcx
-;                     int src_row_size,				;r8
-;                     int dst_row_size)				;r9
+;void sierpinski_asm (unsigned char *src,			;rdi = source
+;                     unsigned char *dst,			;rsi = destino
+;                     int cols, int rows,			;rdx = cols				== a row_size
+													;rcx = filas
+;                     int src_row_size,				;r8  = src row_size		(mismo
+;                     int dst_row_size)				;r9  = dst row_size			  tamaño)
 
 sierpinski_asm:
 
 	push rbp
 	mov rbp, rsp
 	push r12
-	sub rsp,8
-
-													;coef = r12
+	push r13
 
 
-.cicloRows:
+	mov rax, rdx
+	xor r8,r8
+	mov r8, 4
+	div r8
+
+	mul rcx
+	mov r9, rax
+
+									;r9  = contador(en pixeles)
+									;rdx = columnas
+									;rcx = filas
+	xor r12,r12						;r12 = columna(byte) actual
+	xor r13,r13						;r13 = fila actual
+
+.ciclo:
+
+	movq XMM0, r12
+	movq XMM1, r13
+
+	mov r8, 255
+	
 
 
-	.cicloCol:
+	cmp r12, r8
 
-
-		.calcularCoef:
-
-		
-
-
-
-
-
-
-
-
-	add rsp,8
+	pop r13
 	pop r12
 	pop rbp
     ret
