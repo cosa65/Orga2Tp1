ef0f56d44242e2017b77c0b46ee3e92e48e9fcc4
cosa65 <mfosc65@gmail.com>
2014-09-30 00:58:33 -0300

    rge

--- Graficos/chartVar.py
+++ Graficos/chartVar.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 
 
 # Example data
-nombres = ('original','16 acc memoria','16 cuentas')
+nombres = ('original','programas','sin outliers')
 
 y_pos = np.arange(len(nombres))
 
-tiempos = [1621513,2109353,1851460]
+tiempos = [240254,286210,224103]
 
 desviosEstandar = [5533,56191,10864]
 
 colour = ['black','darkgreen','firebrick']
 
+# xerr=desviosEstandar,
 
-
-plt.barh(y_pos, tiempos, xerr=desviosEstandar, align='center', alpha=0.6, color = colour, picker = 1)
+plt.barh(y_pos, tiempos, align='center', alpha=0.6, color = colour, picker = 1)
 plt.yticks(y_pos, nombres)
 plt.xlabel('Tiempos (ciclos)')
 
 for i,j in zip(tiempos,y_pos):
     plt.annotate(str(tiempos[j]),xy=(i,j+0.2))
 
-plt.title('bus vs cpu - Sierpinski')
+plt.title('Esperanzas - Cropflip (10 corridas)')
 
 plt.show()
